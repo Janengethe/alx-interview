aceb8a38ee897bab85c2ffdee670cd0c1c5a7388
Janengethe <janengethew@gmail.com>
2022-04-09 09:53:20 +0300

    spacing

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,7 +12,7 @@ def factorial(n):
         return 1
     if n < 0:
         raise ValueError("No factorial of a negative number")
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         fact = fact * i
     return fact
 
