35be7a7f80e0f1d46454d5e636e1e5735da34e8a
Janengethe <janengethew@gmail.com>
2022-04-09 09:48:39 +0300

    no module imported

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,8 +8,7 @@ Module 0-pascal_triangle
 def factorial(n):
     """factorial of n"""
     fact = 1
-    i = 1
-    for i in range(i, n+1):
+    for i in range(1, n+1):
         fact = fact * i
     return fact
 
